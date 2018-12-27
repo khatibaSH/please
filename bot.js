a79d5c6310e5cfddfc21a8565045347493bce3ff
khatibaSH <45573536+khatibaSH@users.noreply.github.com>
2018-12-27 03:48:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,41 @@ client.on('ready', () => {
 client.user.setGame(`#bc | osama tech`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
-
+const client = new Discord.Client();
+var adminprefix = '&'
+const developers = ['465929376085573634','522045992543977484'] //الاي دى حقك
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+     
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`BY !RG , ZEUS#9999   ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`BY !RG , ZEUS#9699   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`BY !RG , ZEUS#9999   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/One");
+      message.channel.send(`BY !RG , ZEUS#9999`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
 
 
 client.login(process.env.TOKEN);
