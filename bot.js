32d3478b1dffb54a67e272cf9aa646f83147e225
khatibaSH <45573536+khatibaSH@users.noreply.github.com>
2019-01-01 01:05:10 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,12 +23,11 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`#bc | osama tech`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
 var adminprefix = '&'
-const developers = ['465929376085573634'] //الاي دى حقك
+const developers = ['488146324081082378'] //الاي دى حقك
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
